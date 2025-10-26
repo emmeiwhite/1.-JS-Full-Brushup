cc4c35fbd0346af61ef5b738a4c8e9b5e6ce491b
emmeiwhite <emmeiwhite@gmail.com>
2025-10-26 17:50:04 +0530

    4. Template literals & use of expression within these

--- main.js
+++ main.js
@@ -8,3 +8,7 @@ console.log(23 * 'hg')
 
 console.log('This is the first line \nAnd this is the second line')
 console.log('A newline character can be written as "\\n"')
+
+// 3. Template Literals: Apart from being able to span lines, they can also embed other values (even Expressions)
+
+console.log(`Half of 90 is ${90 / 2}`)

82a2733293333c45b18435decd03dcfcae3d8a2a
emmeiwhite <emmeiwhite@gmail.com>
2025-10-26 17:42:06 +0530

    3. Escaping the Character

--- main.js
+++ main.js
@@ -3,3 +3,8 @@
 console.log(0 / 0)
 console.log(Infinity - Infinity)
 console.log(23 * 'hg')
+
+// 2. String Operations
+
+console.log('This is the first line \nAnd this is the second line')
+console.log('A newline character can be written as "\\n"')

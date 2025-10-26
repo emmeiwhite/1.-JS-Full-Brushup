368be7d41bda1a09d975a78627efd8f328b5edf0
emmeiwhite <emmeiwhite@gmail.com>
2025-10-26 17:57:12 +0530

    5. typeof operator

--- main.js
+++ main.js
@@ -12,3 +12,11 @@ console.log('A newline character can be written as "\\n"')
 // 3. Template Literals: Apart from being able to span lines, they can also embed other values (even Expressions)
 
 console.log(`Half of 90 is ${90 / 2}`)
+
+// 4. Not all operator need to have symbols, some have characters like typeof operator
+
+console.log(typeof 434.5)
+console.log(typeof true)
+console.log(typeof 'hi')
+console.log(typeof function () {})
+console.log(typeof {})

16882ec5e36185be42a6a6e38143df986e97767b
emmeiwhite <emmeiwhite@gmail.com>
2025-10-29 19:18:33 +0530

    16. Count Variable also updating

--- advice.js
+++ advice.js
@@ -1,7 +1,7 @@
 console.log('Seek Advice')
 
 // Let's get all our DOM elements
-const advicEl = document.querySelector('.advice')
+const adviceEl = document.querySelector('.advice')
 const countEl = document.querySelector('.count')
 const btnEl = document.querySelector('.btn')
 
@@ -19,7 +19,9 @@ async function getAdvice() {
   console.log(data)
   const { slip } = data
 
-  advicEl.textContent = slip?.advice
+  adviceEl.textContent = slip?.advice
+  count += 1
+  countEl.textContent = count
 }
 
 // btnEl click will call getAdvice function and fetch the advice and render on to the DOM

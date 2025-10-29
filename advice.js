399a1d677c3f4b957c8e42757eb5775fe8ef2232
emmeiwhite <emmeiwhite@gmail.com>
2025-10-29 19:16:25 +0530

    15. Initial Functionality to show advice done!

--- advice.js
+++ advice.js
@@ -3,8 +3,25 @@ console.log('Seek Advice')
 // Let's get all our DOM elements
 const advicEl = document.querySelector('.advice')
 const countEl = document.querySelector('.count')
-const btn = document.querySelector('.btn')
+const btnEl = document.querySelector('.btn')
 
 // let's set the initial state (Bindings)
 let advice = ''
 let count = 0
+
+// Now as the user clicks on the button we'll get an advice from the third party API
+
+const URL = 'https://api.adviceslip.com/advice'
+
+async function getAdvice() {
+  const response = await fetch(URL)
+  const data = await response.json()
+  console.log(data)
+  const { slip } = data
+
+  advicEl.textContent = slip?.advice
+}
+
+// btnEl click will call getAdvice function and fetch the advice and render on to the DOM
+
+btnEl.addEventListener('click', getAdvice)

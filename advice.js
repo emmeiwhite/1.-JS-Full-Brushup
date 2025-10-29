88a409d4920498107efe21936efc08a7562633a1
emmeiwhite <emmeiwhite@gmail.com>
2025-10-29 19:09:20 +0530

    14. Setting and planning the mental model with local state or bindings

--- advice.js
+++ advice.js
@@ -1,1 +1,10 @@
 console.log('Seek Advice')
+
+// Let's get all our DOM elements
+const advicEl = document.querySelector('.advice')
+const countEl = document.querySelector('.count')
+const btn = document.querySelector('.btn')
+
+// let's set the initial state (Bindings)
+let advice = ''
+let count = 0

c64d02f809c9632ede678a7daf9f9aaca308690e
emmeiwhite <emmeiwhite@gmail.com>
2025-11-03 11:02:09 +0530

    18. break statement to immediately jump out of the loop

--- program-structure.js
+++ program-structure.js
@@ -10,3 +10,12 @@ for (let counter = 0; counter < 10; counter++) {
 
 // let counter=0; defines a binding for the state counter, this is the state of the for loop in this case
 console.log(`2 to the power 10 is: `, result)
+
+// Question-2) Find the first number that is both greater than or equal to 20 and divisible by 7
+
+for (let current = 20; ; current++) {
+  if (current % 7 === 0) {
+    console.log(current)
+    break
+  }
+}

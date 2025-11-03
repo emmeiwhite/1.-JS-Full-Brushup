b83587b4a84af2ecc790219898d3819a66962a5b
emmeiwhite <emmeiwhite@gmail.com>
2025-11-03 15:51:15 +0530

    26. Bindings and Scope- pre & post 2015-JS

--- functions/functions.js
+++ functions/functions.js
@@ -6,3 +6,25 @@ console.log('Functions are everywhere, even I am using log function to output to
        3) To associate names with sub-programs
        4) And to isolate these sub-programs from each other
 --- */
+
+const square = x => x * x
+
+console.log(square(5))
+
+// makeNoise() function: A function with 0 parameters
+
+function makeNoise() {
+  console.log('We are here, we are here, we are here!!!')
+}
+
+makeNoise()
+
+// Scope:
+
+let x = 23 // global
+if (true) {
+  let z = 'I am local' // local
+  var k = 'I am global' // global
+}
+
+console.log(k) // k can be accessed anywhere in the program - Pre-2015 JS

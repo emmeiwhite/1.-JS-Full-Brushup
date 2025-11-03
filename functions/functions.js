b81d1664c6802a9500542c416d6cb1506f1b7b83
emmeiwhite <emmeiwhite@gmail.com>
2025-11-03 14:54:01 +0530

    25. Introducing functions as piece of program in a value

---
+++ functions/functions.js
@@ -0,0 +1,8 @@
+console.log('Functions are everywhere, even I am using log function to output to the console')
+
+/* --- Function: Piece of program in a value has many benefits.
+       1) Structure larger prgrams
+       2) To avoid repetition
+       3) To associate names with sub-programs
+       4) And to isolate these sub-programs from each other
+--- */

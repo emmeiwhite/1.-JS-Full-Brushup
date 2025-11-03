b4bd85fac897e8b91c1d255556755d0f6aa67508
emmeiwhite <emmeiwhite@gmail.com>
2025-11-03 12:21:27 +0530

    21. Pattern appended on the DOM

--- Exercises/1.control-structures/1.control-structure.js
+++ Exercises/1.control-structures/1.control-structure.js
@@ -1,10 +1,17 @@
 // Pattern: Right Angled Triangle
+const patternDiv = document.querySelector('.pattern')
 
+let result
 for (let i = 0; i < 7; i++) {
-  let result = '' // resetting to empty string after each outer iteration
+  result = '' // resetting to empty string after each outer iteration
   for (let j = 0; j <= i; j++) {
     result += '# '
   }
 
   console.log(result)
+
+  const para = document.createElement('p')
+  para.textContent = result
+
+  patternDiv.append(para)
 }

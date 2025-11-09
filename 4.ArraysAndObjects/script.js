cda6b82679ba92d2823b56e12ca81ef30fa045cf
emmeiwhite <emmeiwhite@gmail.com>
2025-11-09 15:58:32 +0530

    43. Array - storing sequence of values and retrieving values with indexes

---
+++ 4.ArraysAndObjects/script.js
@@ -0,0 +1,4 @@
+const listOfNumbers = [2, 3, 5, 7, 11] // storing sequence of values
+console.log(listOfNumbers[2]) // 5 Retrieving element 3 from an array
+
+// Arrays are 0 index based. So arr[0] returns the first element.

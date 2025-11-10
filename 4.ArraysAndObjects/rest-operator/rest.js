6aa71e7d72ed738a9c0fa42bb92a2db03872371d
emmeiwhite <emmeiwhite@gmail.com>
2025-11-10 15:39:35 +0530

    57. JSON - serialization format. A Flat Description of data to be sent over the web for communication format.

--- 4.ArraysAndObjects/rest-operator/rest.js
+++ 4.ArraysAndObjects/rest-operator/rest.js
@@ -23,3 +23,41 @@ console.log(max(...numbers))
 // It is possible to include an array like that along with other arguments
 
 max(9, ...numbers, 2) // max(9,5,1,7,2) ->9
+
+// Square bracket array notation similarly allows the triple dot-notation to spread another array into the newArray
+
+let words = ['never', 'fully']
+console.log('will', ...words, 'understand') // → ["will", "never", "fully", "understand"]
+
+// This even words in curly brace objects --- where it adds all properties from another object
+
+let coords = { x: 10, y: 20 }
+console.log({ ...coords, y: 15, z: 1 })
+
+let sindBaadTheSailer = {
+  name: 'Sindbad The Sailer',
+  totalVoyages: 7,
+  sindbadVoyages: [
+    "The Island-Whale and the Roc's Egg",
+    'The Valley of Diamonds and Serpents',
+    'The Island of the Man-Eating Giants',
+    'The Old Man of the Sea',
+    'The Serendib Kingdom and Elephant Graveyard',
+    'The Shipwreck and the Island of Apes',
+    'The Final Journey and Retirement'
+  ]
+}
+
+const updatedSindbad = { ...sindBaadTheSailer, wealth: 'gained' }
+console.log(updatedSindbad)
+const jsonDataForWeb = JSON.stringify(sindBaadTheSailer)
+
+console.log(
+  'The data is serialised to be send over the internet. A flat simple description of data in JSON format is quite a norm.'
+)
+console.log(jsonDataForWeb)
+
+// Assuming that we are receiving the data in JSON format, To work on it, we will need to parse it so that we can use the JS methods and perform operations on the data
+const jsonDataReceived = JSON.parse(jsonDataForWeb)
+
+console.log(jsonDataReceived)

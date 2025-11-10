f24bab37fbc7dcb9c94cee308085f40e549cb539
emmeiwhite <emmeiwhite@gmail.com>
2025-11-10 16:48:39 +0530

    62. sum(range(1,10)) abstracts out the functionality part and programmer has to worry about knowing how these works

---
+++ 5.HigherOrderFunctions/hof.js
@@ -0,0 +1,32 @@
+// Sum problem in two ways: 1-10
+
+let total = 0,
+  count = 1
+
+while (count <= 10) {
+  total += count
+  count++
+}
+
+console.log(total)
+
+// range(3,8) -> [3,4,5,6,7,8]
+function range(start, end) {
+  let arr = []
+  for (let i = start; i <= end; i++) {
+    arr.push(i)
+  }
+  return arr
+}
+
+// sum(arr);
+
+function sum(arr) {
+  let sum = 0
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i]
+  }
+  return sum
+}
+
+console.log(sum(range(1, 10)))

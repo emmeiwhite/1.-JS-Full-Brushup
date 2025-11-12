bec350e78f548865f538549923e399876a0fd393
emmeiwhite <emmeiwhite@gmail.com>
2025-11-12 15:40:35 +0530

    70. HOFs really shine in data processing ...

--- 5.HigherOrderFunctions/hof.js
+++ 5.HigherOrderFunctions/hof.js
@@ -123,3 +123,54 @@ const arrayElems = ['A', 'B', 'C']
 arrayElems.forEach(elem => {
   console.log(elem)
 })
+
+// The most important area where higher-order function shine is data processing.
+// Let's go with this data for SCRIPTS. The binding contains an array of objects, each of which describes a script
+var SCRIPTS = [
+  {
+    name: 'Adlam',
+    ranges: [
+      [125184, 125259],
+      [125264, 125274],
+      [125278, 125280]
+    ],
+    direction: 'rtl',
+    year: 1987,
+    living: true,
+    link: 'https://en.wikipedia.org/wiki/Fula_alphabets#Adlam_alphabet'
+  },
+  {
+    name: 'Caucasian Albanian',
+    ranges: [
+      [66864, 66916],
+      [66927, 66928]
+    ],
+    direction: 'ltr',
+    year: 420,
+    living: false,
+    link: 'https://en.wikipedia.org/wiki/Caucasian_Albanian_alphabet'
+  },
+  {
+    name: 'Ahom',
+    ranges: [
+      [71424, 71450],
+      [71453, 71468],
+      [71472, 71488]
+    ],
+    direction: 'ltr',
+    year: 1250,
+    living: false,
+    link: 'https://en.wikipedia.org/wiki/Ahom_alphabet'
+  }
+]
+
+// Question now is all about processing data, Right and that too with HOFs.
+
+// Question-1) find the scripts in the data set that are still in use?
+
+const livingScripts = SCRIPTS.filter(script => {
+  return script.living
+})
+
+console.log('The living scripts are: ')
+console.log(livingScripts)
